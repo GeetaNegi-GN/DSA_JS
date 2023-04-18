192315d12e9e3497ad8bec7eba88a46d4a16fb65
Geeta Negi <geetanegi550@gmail.com>
2023-04-19 01:41:08 +0530

    done

--- Array/basic.js
+++ Array/basic.js
@@ -1,3 +1,17 @@
 const strings = ['a' ,'b','c','d'];
 //4*4 = 16 bytes of storage
-strings[2]
+
+strings[2];//O(1)
+//push
+strings.push('e');
+//pop
+strings.pop();
+strings.pop(); //O(1)
+
+//unshift
+strings.unshift('x');//O(n)
+
+//splice
+strings.splice(2,0,'alien')//O(n/2) => O(n)
+
+console.log(strings)
---
+++ Array/reverseString.js
@@ -0,0 +1,8 @@
+//function that reverse a string:
+function reverse(str){
+    if(!str || str.length <2 || typeof str !== 'string'){
+        return "this is not goood input";
+    }
+    const backwords = [];
+    const 
+}
