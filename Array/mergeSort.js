d31d7aea4b626e9275c64b394c22578ea21ce50c
Geeta Negi <geetanegi550@gmail.com>
2023-04-23 18:56:27 +0530

    array complete

---
+++ Array/mergeSort.js
@@ -0,0 +1,33 @@
+//mergeSortedArrays([0,3,4,31],[4,6,30]);
+//[0,3,4,4,6,30,31]
+function mergeSortedArrays(array1,array2){
+    const mergedArray = [];
+    let array1item = array1[0];
+    let array2item = array2[0];
+    let i = 1;
+    let j= 1;
+    //check input
+    if(array1.length === 0){
+        return array2;
+
+    }
+    if(array2.length === 0){
+        return array1;
+    }
+
+    while (array1item || array2item){
+        if(!array2item || array1item<array2item){
+        mergedArray.push(array1item)
+        array1item = array1[i];
+        i++;
+        }
+        else{
+            mergedArray.push(array2item)
+            array2item=array2[j];
+            j++;
+        }
+    }
+    return mergedArray;
+}
+
+mergeSortedArrays([0,3,4,31],[4,6,30]);
--- Array/reverseString.js
+++ Array/reverseString.js
@@ -3,6 +3,18 @@ function reverse(str){
     if(!str || str.length <2 || typeof str !== 'string'){
         return "this is not goood input";
     }
-    const backwords = [];
-    const 
+    const backwards = [];
+    const totalItems =str.length -1 ;
+    for(let i =totalItems; i>= 0;i--){
+        backwards.push(str[i]);
+    }
+    console.log(backwards);
+    return backwards.join('');
 }
+function reverse2(str){
+    return str.split('').reverse().join('')
+}
+const reverse3 = str => [...str].reverse().join('');
+reverse('this is great')
+reverse2('good to see you');
+reverse3('hello My Name is Geeta Negi');
