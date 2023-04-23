0a3d6b7995f6ac236d3d7b1c08dbfbfeb4fc2121
Geeta Negi <geetanegi550@gmail.com>
2023-04-23 18:56:03 +0530

    array complete

---
+++ hashTable/hashBasics.js
@@ -0,0 +1,13 @@
+let user = {
+    age: 25,
+    name:'Geeta',
+    magic: true,
+    scream: function(){
+        console.log('hahahaa!');
+
+    }
+}
+user.age //O(1)
+user.scream();//O(1)
+user.spell = 'hello';//O(1)
+user//O(1)
